9c9a37082f457e4b22fa00cd65b47812f664ad6d
Dongi <chaedonggi@gmail.com>
2022-04-03 19:26:59 +0900

    First styled component

--- src/App.js
+++ src/App.js
@@ -3,27 +3,23 @@ import styled from "styled-components";
 const Father = styled.div`
   display: flex;
 `;
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-const BoxTwo = styled.div`
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   background-color: tomato;
   width: 100px;
   height: 100px;
 `;
-const Text = styled.span`
-  color: white;
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
+
+
 
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
     </Father>
   );
 }
